367b5908e7104e990bce2b4bb49c69e875011a09
tanima <tanima787@gmail.com>
2023-08-14 14:40:50 +0530

    updated

---
+++ index.java
@@ -0,0 +1,11 @@
+import java .util.*;
+import java.io.*;
+public class index{
+    public static void main(String args[]){
+    int a=2;
+    int b=3;
+     a=a+b;
+     System.out.println(a);
+
+    }
+}

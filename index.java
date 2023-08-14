7d1388f941303b844ff36f8ac1bf5ee77342613e
tanima1231 <66523673+tanima1231@users.noreply.github.com>
2023-08-14 16:17:54 +0530

    Updated 6

--- index.java
+++ index.java
@@ -2,7 +2,7 @@ import java .util.*;
 import java.io.*;
 public class index{
     public static void main(String args[]){
-    int a=2;
+    int a=6;
     int b=5;
      a=a+b;
      System.out.println(a);
